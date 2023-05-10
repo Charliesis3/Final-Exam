4a8bbc453d413996d2ccf8b1f6bfc658a18ec637
Charliesis3 <124640597+Charliesis3@users.noreply.github.com>
2023-05-09 23:53:43 -0400

    Add files via upload

---
+++ final.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 23:51:57 2023
+
+@author: charliesiciliano
+"""
+
+import pandas as pd
+import numpy as np
+
+# Read in the data
+gas_prices = pd.read_csv("gasoline_prices.csv")
+state_codes = pd.read_csv("state_abb_codes.csv")
+census_regions = pd.read_csv("censusRegions.csv")
+
+# Merge data
+StateCode_GasPrice = pd.merge(state_codes, gas_prices, on='State')
+
+# Create data frame with state code name and corresponding regular and diesel prices
+type_gas = ["Code", "Regular", "Diesel"]
+gp_name = StateCode_GasPrice[type_gas]
+
+# Merge data with census_regions to include regions
+gp_ByRegion = pd.merge(gp_name, census_regions, left_on='Code', right_on='State')
+
+# Remove Washington DC
+gp_ByRegion_NoDC = gp_ByRegion[gp_ByRegion['Code'] != 'DC']
+
+# Group by region and calculate percent increase, standard deviation, and coefficient of variation
+Result = gp_ByRegion_NoDC.groupby(['Region']).agg(mean_pct_increase=('Diesel', 'mean'), 
+                                                  sd_pct_increase=('Diesel', 'std'),
+                                                  coef_pct_increase=('Diesel', lambda x: np.std(x) / np.mean(x)),
+                                                 )
+print(Result)
